0c84d6c8ac017be294871eaac8b70986952e6df9
hnt-neptunium <kw.chaney2003@gmail.com>
2022-01-11 16:06:05 -0600

    🤏 small optimizations
    fuck you citizen.

--- distritic-nogrip/nogrip.lua
+++ distritic-nogrip/nogrip.lua
@@ -1,27 +1,23 @@
--- this is script edited by neptunium-dev!
--- show love: https://forum.cfx.re/u/distritic
-startMsg = false -- client alert message, fasle by default.
-local ragdoll_chance = 0.8 -- (50 = 0.5; 75 = 0.75; etc).
+startMsg = false -- fasle by default.
+local ragdoll_chance = 0.8 -- (50 = 0.5; 75 = 0.75)
 
-Citizen.CreateThread(function()
+if startMsg then
+    print("nogrip is enabled for this server.")
+end
+
+CreateThread(function()
 	while true do
-		Citizen.Wait(100)
+		Wait(100)
 		local ped = PlayerPedId()
 		if IsPedOnFoot(ped) and not IsPedSwimming(ped) and (IsPedRunning(ped) or IsPedSprinting(ped)) and not IsPedClimbing(ped) and IsPedJumping(ped) and not IsPedRagdoll(ped) then
 			local chance_result = math.random()
 			if chance_result < ragdoll_chance then 
-				Citizen.Wait(600)
+				Wait(600)
                 ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 0.08)
 				SetPedToRagdoll(ped, 5000, 1, 2)
 			else
-				Citizen.Wait(1500)
+				Wait(1500)
 			end
 		end
 	end
 end)
-
-if startMsg then
-    print("nogrip is enabled for this server.")
-else
-    -- do nothing, no message required!
-end
